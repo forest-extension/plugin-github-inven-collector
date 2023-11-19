d07b9cef3abd381edb24317d41c6d03753cbaffc
Yongtae Park <samuel.park@megazone.com>
2023-11-19 17:15:34 +0900

    feat: set main.py

--- src/plugin/main.py
+++ src/plugin/main.py
@@ -1,66 +1,39 @@
 from spaceone.inventory.plugin.collector.lib.server import CollectorPluginServer
 
+from plugin.manager.repository_manager import RepositoryManager
 
 app = CollectorPluginServer()
 
 
 @app.route('Collector.init')
 def collector_init(params: dict) -> dict:
-    """ init plugin by options
-
-    Args:
-        params (CollectorInitRequest): {
-            'options': 'dict',    # Required
-            'domain_id': 'str'
-        }
-
-    Returns:
-        PluginResponse: {
-            'metadata': 'dict'
-        }
-    """
-    pass
-
-
-@app.route('Collector.verify')
-def collector_verify(params: dict) -> None:
-    """ Verifying collector plugin
-
-    Args:
-        params (CollectorVerifyRequest): {
-            'options': 'dict',      # Required
-            'secret_data': 'dict',  # Required
-            'schema': 'str',
-            'domain_id': 'str'
-        }
-
-    Returns:
-        None
-    """
-    pass
+    return {'metadata': {'options_schema': _create_options_schema()}}
 
 
 @app.route('Collector.collect')
 def collector_collect(params: dict) -> dict:
-    """ Collect external data
+    options = params['options']
+    secret_data = params['secret_data']
+    schema = params.get('schema')
 
-    Args:
-        params (CollectorCollectRequest): {
-            'options': 'dict',      # Required
-            'secret_data': 'dict',  # Required
-            'schema': 'str',
-            'domain_id': 'str'
-        }
+    repository_manager = RepositoryManager()
+    return repository_manager.collect_resources(options, secret_data, schema)
 
-    Returns:
-        Generator[ResourceResponse, None, None]
-        {
-            'state': 'SUCCESS | FAILURE',
-            'resource_type': 'inventory.CloudService | inventory.CloudServiceType | inventory.Region',
-            'resource_data': 'dict',
-            'match_keys': 'list',
-            'error_message': 'str'
-            'metadata': 'dict'
+
+def _create_options_schema():
+    return {
+        'required': ['items'],
+        'order': ['items'],
+        'type': 'object',
+        'properties': {
+            'items': {
+                'title': 'Item filter',
+                'type': 'array',
+                'items': {
+                    'enum': [
+                        'fields'
+                    ]
+                }
+            }
         }
-    """
-    pass
+    }
